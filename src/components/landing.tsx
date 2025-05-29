7ff950d2a294d69f800830636e9daa40c006d651
Anirudh Jwala <anirudhsai2@gmail.com>
2025-05-30 00:21:05 +0530

    feat: add speaker images and implement scrolling animation in Landing component

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,11 +1,19 @@
 import Hero from "@/components/hero";
 import Navbar from "@/components/nav-bar";
 import heroBackground from "@/assets/hero-background.png";
+import speaker1 from "@/assets/speaker-1.png";
+import speaker2 from "@/assets/speaker-2.png";
+import speaker3 from "@/assets/speaker-3.png";
+import speaker4 from "@/assets/speaker-4.png";
+import speaker5 from "@/assets/speaker-5.png";
+import speaker6 from "@/assets/speaker-6.png";
+
+const speakers = [speaker1, speaker2, speaker3, speaker4, speaker5, speaker6];
 
 const Landing = () => {
   return (
     <div
-      className="flex flex-col justify-start items-center font-display h-[1280px] w-full"
+      className="relative flex flex-col justify-start items-center font-display h-[1280px] w-full overflow-hidden"
       style={{
         backgroundImage: `url(${heroBackground})`,
         backgroundSize: "cover",
@@ -13,6 +21,18 @@ const Landing = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
+      <div className="absolute bottom-0 w-full overflow-hidden h-[440px] pointer-events-none">
+        <div className="animate-scroll flex w-max gap-2">
+          {[...speakers, ...speakers].map((src, i) => (
+            <img
+              key={i}
+              src={src}
+              alt={`speaker-${i}`}
+              className="h-[28rem] w-[28rem] object-contain"
+            />
+          ))}
+        </div>
+      </div>
       <Navbar />
       <Hero />
     </div>
