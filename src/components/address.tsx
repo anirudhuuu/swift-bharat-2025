6bb055e6c73713a9bc5678a4991f2713d850f91b
Anirudh Jwala <anirudhsai2@gmail.com>
2025-05-29 23:53:03 +0530

    refactor: improve layout and typography in Address component

--- src/components/address.tsx
+++ src/components/address.tsx
@@ -1,33 +1,34 @@
-import { Separator } from "@/components/ui/separator";
 import dateAddressBackground from "@/assets/date-address-background.png";
+import Heading from "@/components/heading";
+import { Separator } from "@/components/ui/separator";
 
 const Address = () => {
   return (
     <div
-      className="flex flex-col w-full font-display min-h-[590px]"
+      className="flex flex-col w-full font-display min-h-[550px]"
       style={{
         backgroundImage: `url(${dateAddressBackground})`,
         backgroundSize: "cover",
-        backgroundPosition: "0% 100%",
+        backgroundPosition: "0% 60%",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="max-w-2xl mx-auto mt-12 px-6 md:px-12 text-black">
-        <h2 className="uppercase font-normal text-base lg:text-lg text-black/60 mb-4">
+      <div className="max-w-xl mx-auto mt-12 px-6 md:px-12 text-black">
+        <h2 className="uppercase font-medium text-base lg:text-lg text-black/60 mb-4">
           Dates
         </h2>
-        <p className="font-semibold text-3xl lg:text-5xl leading-tight">
+        <Heading fontWeight="semibold" mdFontSize="text-heading-2">
           15th-17th
           <br />
           September, 2025
-        </p>
+        </Heading>
         <Separator className="my-10 bg-black/20" />
-        <h2 className="uppercase font-normal text-base lg:text-lg text-black/60 mb-4">
+        <h2 className="uppercase font-medium text-base lg:text-lg text-black/60 mb-4">
           Address
         </h2>
-        <p className="font-semibold text-3xl lg:text-5xl leading-tight">
+        <Heading fontWeight="semibold" mdFontSize="text-heading-2">
           Bangalore International Convention Center
-        </p>
+        </Heading>
       </div>
     </div>
   );
