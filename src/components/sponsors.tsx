7ed9a753f636042ef11a7c88ef6d5dbab8a2b398
anirudhsai2 <anirudhsai2@gmail.com>
2025-08-24 10:56:54 +0530

    feat: add sponsor-divider SVG and update Sponsors component to include divider images and improved styling

--- src/components/sponsors.tsx
+++ src/components/sponsors.tsx
@@ -1,4 +1,5 @@
 import sponsorsBackground from "@/assets/green-lush-background.png";
+import sponsorDivider from "@/assets/svg/sponsor-divider.svg";
 
 const Sponsors = () => {
   return (
@@ -8,13 +9,55 @@ const Sponsors = () => {
       style={{
         backgroundImage: `url(${sponsorsBackground})`,
         backgroundSize: "cover",
-        backgroundPosition: "right center",
+        backgroundPosition: "50% center",
         backgroundRepeat: "no-repeat",
         minHeight: "800px",
         maxHeight: "2200px",
       }}
     >
-      <h2 className="font-bold text-5xl text-center mb-16">Sponsors</h2>
+      <h2 className="font-bold text-3xl sm:text-5xl text-center mb-16">
+        Sponsors
+      </h2>
+      <div className="w-full my-4 flex flex-col items-center justify-center relative">
+        <div className="absolute top-1/2 left-1/2 bg-[#FBDD93] px-4 -translate-x-1/2 -translate-y-1/2 z-10 font-thin uppercase text-lg text-black/60">
+          Platinum Sponsor
+        </div>
+        <img
+          src={sponsorDivider}
+          alt="Divider"
+          className="h-6 sm:h-8 w-full object-contain"
+        />
+      </div>
+      <div className="w-full my-4 flex flex-col items-center justify-center relative">
+        <div className="absolute top-1/2 left-1/2 bg-[#FBDD93] px-4 -translate-x-1/2 -translate-y-1/2 z-10 font-thin uppercase text-lg text-black/60">
+          Gold Sponsor
+        </div>
+        <img
+          src={sponsorDivider}
+          alt="Divider"
+          className="h-6 sm:h-8 w-full object-contain"
+        />
+      </div>
+      <div className="w-full my-4 flex flex-col items-center justify-center relative">
+        <div className="absolute top-1/2 left-1/2 bg-[#FBDD93] px-4 -translate-x-1/2 -translate-y-1/2 z-10 font-thin uppercase text-lg text-black/60">
+          Silver Sponsors
+        </div>
+        <img
+          src={sponsorDivider}
+          alt="Divider"
+          className="h-6 sm:h-8 w-full object-contain"
+        />
+      </div>
+      <div className="w-full my-4 flex flex-col items-center justify-center relative">
+        <div className="absolute top-1/2 left-1/2 bg-[#FBDD93] px-4 -translate-x-1/2 -translate-y-1/2 z-10 font-thin uppercase text-lg text-black/60">
+          Community Sponsors
+        </div>
+        <img
+          src={sponsorDivider}
+          alt="Divider"
+          className="h-6 sm:h-8 w-full object-contain"
+        />
+      </div>
     </section>
   );
 };
